7908447d4783aa367b1ad56da1f943759c034225
Jana Enigma Baruc <janaenigma.baruc@GXCHLAP00689.local>
2025-07-01 15:11:18 +0800

    Final Submisison, Lab 1: Celsius to Fahrenheit Converter

---
+++ Readme.md
@@ -0,0 +1,10 @@
+### Baruc, Jana Enigma S.
+
+# Celsius to Fahrenheit Converter
+
+### Sample Output
+
+![Sample Output](sample%20output.png)
+
+### Test Validation
+![Test Validationn](test%20validation.png)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
 package org.example;
 
-import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.print("Enter temperature in Celsius: ");
 
+        float tempCel = myObj.nextFloat();
+        double tempFar = tempCel * 1.8 + 32;
+        System.out.printf("Temperature in Fahrenheit: " + "%.2f%n", tempFar);
     }
 }
